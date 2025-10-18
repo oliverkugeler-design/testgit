f6f2b8051bb8eff74934f54aae62ed715e9e57b8
oliver.kugeler <oliver.kugeler@gmail.com>
2025-10-18 21:22:36 +0200

    test-commit

---
+++ HelloWorld.py
@@ -0,0 +1,23 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+
+def main():
+    app = QApplication(sys.argv)
+    
+    # Create a window
+    window = QWidget()
+    window.setWindowTitle("Hello PyQt")
+    window.setGeometry(100, 100, 300, 100)  # x, y, width, height
+    
+    # Add a label to the window
+    label = QLabel("Hello, World!", parent=window)
+    label.move(100, 40)
+    
+    # Show window
+    window.show()
+    
+    # Enter the Qt event loop
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
