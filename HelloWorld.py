abcc745795fce353e66e04c03db5c80bacbaddc4
Oliver Kugeler <oliver.kugeler@gmail.com>
2025-10-19 20:29:34 +0200

    migrated to Pyqt6

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,23 +1,27 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout
 
 def main():
     app = QApplication(sys.argv)
-    
-    # Create a window
+
+    # Create the main window
     window = QWidget()
-    window.setWindowTitle("Hello PyQt !")
-    window.setGeometry(100, 100, 300, 100)  # x, y, width, height
-    
-    # Add a label to the window
-    label = QLabel("Hallo Welt 123", parent=window)
-    label.move(100, 40)
-    
+    window.setWindowTitle("Hello World - PyQt6")
+
+    # Add a label
+    label = QLabel("Hello, World!")
+    label.setStyleSheet("font-size: 24px; color: darkblue;")
+
+    # Layout
+    layout = QVBoxLayout()
+    layout.addWidget(label)
+    window.setLayout(layout)
+
     # Show window
+    window.resize(300, 100)
     window.show()
-    
-    # Enter the Qt event loop
-    sys.exit(app.exec_())
+
+    sys.exit(app.exec())
 
 if __name__ == "__main__":
     main()
